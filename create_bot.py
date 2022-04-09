6105443dc691843cfeae3a56600c42e4c89496ba
holmsya <iholmsya@gmail.com>
2022-04-09 13:38:18 +0300

    kill token

--- create_bot.py
+++ create_bot.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage    # позволяет хранить данные в ОЗУ
 
-API_TOKEN = '5191483081:AAHFy4leaeuA_wWEP0HdKgs7smimsE6oYLA'
+API_TOKEN = ''
 
 storage = MemoryStorage()
 bot = Bot(token=API_TOKEN)
